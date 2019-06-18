4d1905007febb2d4ac4d3a413b3502b89e1d7885
Geripii <33180486+Geripii@users.noreply.github.com>
2019-06-18 20:29:51 +0100

    Set Up

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = os.getenv('MONGO_URI','mongodb+srv://admin:admin@cluster0-c9bjx.mongodb.net/task_manager?retryWrites=true&w=majority')
+app.config["MONGO_URI"] = os.getenv('MONGO_URI',
+                                    'mongodb+srv://admin:admin@cluster0-c9bjx.mongodb.net/task_manager?retryWrites=true&w=majority')
 
 mongo = PyMongo(app)
 
@@ -15,10 +16,17 @@ mongo = PyMongo(app)
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
+
 @app.route('/add_task')
 def add_task():
     return render_template("addtask.html", categories=mongo.db.categories.find())
 
+
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
